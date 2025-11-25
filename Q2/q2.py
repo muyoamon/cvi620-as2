85d33f003199b152ca29a36576d6d5a49dbe11f9
Thanatorn Wongthanaporn <muyoamon@gmail.com>
2025-11-25 15:00:37 -0500

    q2

--- Q2/q2.py
+++ Q2/q2.py
@@ -19,14 +19,9 @@ def load_data(path: str):
         label = row[0]
         img_data = row[1:].values.astype(np.uint8).reshape(28, 28)
 
-        # Resize image to 256x256
-        img_resized = cv2.resize(img_data, (256, 256))
 
-        data_list.append(img_resized)
+        data_list.append(img_data)
         labels.append(label)
-
-    
-    
 
     return data_list, labels
 
@@ -35,37 +30,25 @@ def load_all_data():
     train_data, train_labels = load_data('Q2/mnist_train.csv')
     test_data, test_labels = load_data('Q2/mnist_test.csv')
 
-    x_train = np.array(train_data)
-    x_test = np.array(test_data)
+    x_train = np.array(train_data).astype("float32") / 255
+    x_test = np.array(test_data).astype("float32") / 255
     y_train = np.array(train_labels)
     y_test = np.array(test_labels)
 
     return x_train, x_test, y_train, y_test
 
-def get_cnn_model(input_shape=(256, 256, 3)):
+def get_cnn_model(input_shape=(28, 28, 1)):
     model = Sequential([
-        layers.Conv2D(4, (3, 3), activation="relu", input_shape=input_shape),
-        layers.BatchNormalization(),
+        layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape),
         layers.MaxPool2D((2,2)),
-        layers.Dropout(0.25),
 
 
-        layers.Conv2D(8, (3, 3), activation="relu"),
-        layers.BatchNormalization(),
+        layers.Conv2D(64, (3, 3), activation="relu"),
         layers.MaxPool2D((2,2)),
-        layers.Dropout(0.25),
 
-
-        layers.Conv2D(16, (3, 3), activation="relu"),
-        layers.BatchNormalization(),
-        layers.MaxPool2D((2,2)),
-        layers.Dropout(0.25),
 
         layers.Flatten(),
-        layers.Dense(16, activation='relu'),
-        layers.BatchNormalization(),
-        layers.Dropout(0.5),
-        
+        layers.Dense(64, activation='relu'),
         layers.Dense(10, activation='softmax'),
     ])
 
@@ -77,40 +60,28 @@ def get_cnn_model(input_shape=(256, 256, 3)):
 
     return model
 
-model = get_cnn_model(input_shape=(256,256, 3))
+model = get_cnn_model(input_shape=(28,28, 1))
 model.summary()
-
-
-
-# -- KNN --
-# DATA
-x_train, x_test, y_train, y_test = load_all_data()
-
-# MODEL
-clf = KNeighborsClassifier(n_neighbors=5)
-clf.fit(x_train, y_train)
-knn_predictions = clf.predict(x_test)
-
-#EVAL
-knn_accuracy = accuracy_score(y_test, knn_predictions)
 
 
 # -- CNN --
 #DATA
-x_train, x_test, y_train, y_test = load_all_data(flatten=False);
+x_train, x_test, y_train, y_test = load_all_data();
 le = LabelEncoder();
 
 y_test = le.fit_transform(y_test)
 y_train = le.fit_transform(y_train)
 
-y_test = to_categorical(y_test)
-y_train = to_categorical(y_train)
+y_test = to_categorical(y_test, num_classes=10)
+y_train = to_categorical(y_train, num_classes=10)
 
 
 
 
 # MODEL
-H = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=16)
+H = model.fit(x_train, y_train, epochs=5, batch_size=8, validation_data=(x_test, y_test))
+
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose = 0)
 
 
 
@@ -119,12 +90,12 @@ H = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=16)
 plt.figure(figsize =(10,5))
 plt.plot(H.history['accuracy'], label='Training Accuracy')
 plt.plot(H.history['val_accuracy'], label='Validation Accuracy')
-plt.title('Model Accuracy')
+plt.title('Q2 CNN Model Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
-plt.savefig('CNN_res.png')
+plt.savefig('q2_CNN_res.png')
 
-print(f" KNN accuracy: {knn_accuracy * 100}%")
-print(f" CNN accuracy: {H.history['val_accuracy'][-1] * 100}%")
+print(f" CNN accuracy: {test_acc * 100}%")
+print(f" CNN loss: {test_loss * 100}%")
